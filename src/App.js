ad93cf73ad28aca23d9400c0db960db587c6c940
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2021-06-28 09:13:10 +0700

    Resolve Merge Conflicts & Fix products in Sidebar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
-import NavbarCustom from './components/layout/Navbar';
-import Slidebar from './components/layout/Slidebar';
-<<<<<<< .mine
-
-=======
-import RouterURL from './RouterURL'
->>>>>>> .theirs
+import NavbarCustom from "./components/layout/Navbar";
+import Slidebar from "./components/layout/Slidebar";
+import RouterURL from "./RouterURL";
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <NavbarCustom/>
-      <Slidebar/>
-		<Dashboard/>
-      <RouterURL/>
-    </div>
+      <div className="App">
+        <NavbarCustom />
+        <Slidebar />
+        <Dashboard />
+        <RouterURL />
+      </div>
     </BrowserRouter>
   );
 }
